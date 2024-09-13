cef9f6f5de3471dc0fd12c5bb80a1bb9af8e75fb
Kevin Bu <kbpi314@gmail.com>
2024-09-13 16:49:48 -0400

    daily update, heatmap annotations

--- code/DGCA_heatmap.R
+++ code/DGCA_heatmap.R
@@ -25,10 +25,12 @@ for (y in c('saliva', 'stool')){
   } else if (y == 'saliva'){
     inputs = c('outputs/jobs15/data_processing/summary_df_resample_1.txt')
   }
+  metadata = read.table(file=paste('inputs/df_quant_',y,'.tsv',sep=''),sep='\t',header=T, row.names=1, stringsAsFactors=F, check.names=F,comment.char='@')
   metadata = read.table(file=paste('inputs/cutie_df_abun_',y,'.tsv',sep=''),sep='\t',header=T, row.names=1, stringsAsFactors=F, check.names=F,comment.char='@')
+  metadata = read.table(file=paste('inputs/cutie_df_taxa_',y,'.tsv',sep=''),sep='\t',header=T, row.names=1, stringsAsFactors=F, check.names=F,comment.char='@')
   
   # Compile Cutie results and calculate corr difference ----------------
-  res_dir = '~/Desktop/clemente_lab/Projects/twinsra/outputs/jobs19'
+  res_dir = '~/Desktop/clemente_lab/Projects/oa/'
   # cutie_res = {}
   # grab first input
   fp = inputs[1]
@@ -38,6 +40,8 @@ for (y in c('saliva', 'stool')){
   filt = sapply(df[1:2], function(x) x %in% c(colnames(metadata)))
   
   # keep columns 1, 2 and 4 and keep rows where one of the two entries is in the metadata file list of vars
+  # keep columns 1, 2 and 4 and keep rows where BOTH of the two entries is in the metadata file list of vars
+  # df_filt = df[apply(filt, 2, sum)==1, c(1:2, 4)]
   df_filt = df[apply(filt, 1, sum)==1, c(1:2, 4)]
   corr_matrix = spread(df_filt, var2, correlations) %>%
     'rownames<-'(.[, 1]) %>% .[, -1]
@@ -52,23 +56,30 @@ for (y in c('saliva', 'stool')){
   # Heatmap -----------------------------------------------------------------
   # Ref: https://jokergoo.github.io/ComplexHeatmap-reference/book/introduction.html
   #corr_diff <- corr_diff[,colSums(is.na(corr_diff))<nrow(corr_diff)]
-  corr_diff <- corr_diff[rowSums(is.na(corr_diff))<ncol(corr_diff),]
-  corr_diff[is.na(corr_diff)] <- 0
+  #corr_diff <- corr_diff[rowSums(is.na(corr_diff))<ncol(corr_diff),]
+  #corr_diff[is.na(corr_diff)] <- 0
   data_matrix = as.matrix(corr_diff) # %>% na.omit
   # is replacing with 0 valid?
   #dend = as.dendrogram(hclust(dist(data_matrix), 
   #                            method='average'))
 
   # create annotations
-  #df_anno = read.table(file=paste('inputs/df_quant_R_corr_labels.tsv',sep=''),sep='\t',header=T)#,row.names=1)#,header=T, row.names=1, stringsAsFactors=F, check.names=F,comment.char='@')
-  #cols_anno = df_anno$var_type
+  df_anno_cols = read.table(file=paste('inputs/col_',y,'.tsv',sep=''),sep='\t',header=T)#,row.names=1)#,header=T, row.names=1, stringsAsFactors=F, check.names=F,comment.char='@')
+  cols_anno = df_anno_cols$var_type
+  
+  df_anno_rows = read.table(file=paste('inputs/row_',y,'.tsv',sep=''),sep='\t',header=T)#,row.names=1)#,header=T, row.names=1, stringsAsFactors=F, check.names=F,comment.char='@')
+  rows_anno = df_anno_rows$var_type
   
   #Create a custom color scale
   # https://stackoverflow.com/questions/6919025/how-to-assign-colors-to-categorical-variables-in-ggplot2-that-have-stable-mappin
   library(RColorBrewer)
-  myColors <- brewer.pal(8,"Set1")
-  #names(myColors) <- levels(df_anno$var_type)
-  colScale <- scale_colour_manual(name = "Feature Type",values = myColors)
+  col_colors <- brewer.pal(length(unique(cols_anno)),"Set1")
+  names(col_colors) <- levels(df_anno_cols$var_type)
+  col_scale <- scale_colour_manual(name = "Feature Type",values = col_colors)
+
+  row_colors <- brewer.pal(length(unique(rows_anno)),"Set1")
+  names(row_colors) <- levels(df_anno_rows$var_type)
+  row_scale <- scale_colour_manual(name = "Feature Type",values = row_colors)
   
   # remove nearzerovar
   col_idx <- nearZeroVar(data_matrix)
@@ -76,54 +87,61 @@ for (y in c('saliva', 'stool')){
     data_matrix <- data_matrix
   } else {
     data_matrix <- data_matrix[,-c(col_idx)]
-    # cols_anno <- cols_anno[-c(col_idx)]
+    cols_anno <- cols_anno[-c(col_idx)]
   }
   # set color annotation
   # https://www.biostars.org/p/317349/
-  #ann <- data.frame(cols_anno)
-  #colnames(ann) <- c('Type')#, 'Type2')
-  #colours <- list('Type' = c('Clinical' = myColors[1], 
-  #                           'Plasma_Autoantibodies' = myColors[2],
-  #                           'Fecal_Autoantibodies' = myColors[3],
-  #                           'Plasma_Cytokines' = myColors[4],
-  #                           'Serum_Fatty_Acids' = myColors[5],
-  #                           'Stool_Fatty_Acids' = myColors[6],
-  #                           'Metagenomic_Taxa' = myColors[7],
+  col_ann <- data.frame(cols_anno)
+  colnames(col_ann) <- c('Type')#, 'Type2')
+  col_colours <- list('Type' = c('Sig' = myColors[1], 
+                             'Nonsig' = myColors[2]))
   #                           'Metagenomic_Pathways' = myColors[8]))
   #  
-  #colAnn <- HeatmapAnnotation(df = ann,
-  ##                            which = 'col',
-  #                            col = colours,
-  #                            annotation_width = unit(c(1, 4), 'cm'),
-  #                            gap = unit(1, 'mm'))
+  colAnn <- HeatmapAnnotation(df = col_ann,
+                              which = 'col',
+                              col = col_colours,
+                              annotation_width = unit(c(1, 4), 'cm'),
+                              gap = unit(1, 'mm'))
+  
+  row_ann <- data.frame(rows_anno)
+  colnames(row_ann) <- c('Type')#, 'Type2')
+  row_colours <- list('Type' = c('BA' = myColors[1], 
+                             'NonBA' = myColors[2]))
+  #                           'Metagenomic_Pathways' = myColors[8]))
+  #  
+  rowAnn <- HeatmapAnnotation(df = row_ann,
+                              which = 'row',
+                              col = row_colours,
+                              annotation_width = unit(c(1, 4), 'cm'),
+                              gap = unit(1, 'mm'))
   
   
   file_path <- paste('/Users/KevinBu/Desktop/clemente_lab/Projects/oa/outputs/jobs17/heatmap_corr_',y,'.pdf',sep='')
   pdf(GetoptLong::qq(file_path), width = 8, height=8)  # Image size
-  hm <- Heatmap(data_matrix, use_raster = FALSE,
+  hm <- Heatmap(data_matrix, use_raster = FALSE, name='Spearman',
                 width = unit(8, "cm"),
                 row_dend_width = unit(1.5, "cm"),
                 
                 clustering_method_rows = "average",  # UPGMA
                 clustering_method_columns = "average",
                 
-                # row_title = y,
+                row_title = 'Metabolites',
                 row_names_gp = gpar(fontsize = 4),
                 show_row_names = FALSE,
                 
-                # column_title = 'Quant Vars',
+                column_title = 'Taxa',
                 show_column_names = FALSE,
                 column_names_gp = gpar(fontsize = 4),
                 
-                # top_annotation = colAnn, #columnAnnotation(VarType=cols_anno, col=colScale),
-                
+                top_annotation = colAnn, #columnAnnotation(VarType=cols_anno, col=colScale),
+                left_annotation = rowAnn,
                 #heatmap_legend_param = list(
                 #  title = title,
                 #  direction = "horizontal"
                 #)
   )
   
-  hm = draw(
+  hm_draw = draw(
     hm, background = "transparent",
     heatmap_legend_side="bottom", 
     annotation_legend_side="right"
